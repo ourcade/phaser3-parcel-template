e17803ea3e3ace56ade8858fe1d5c265af9f10e4
Nicolas Nocete <nicolasnocete@gmail.com>
2022-09-07 16:17:54 -0300

    Se agregar caracteristicas de escalado

--- src/main.js
+++ src/main.js
@@ -6,10 +6,23 @@ const config = {
 	type: Phaser.AUTO,
 	width: 800,
 	height: 600,
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+		min: {
+			width: 800,
+			height: 600,
+		},
+		max: {
+			width: 1600,
+			height: 1200,
+		},
+	},
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 200 }
+			gravity: { y: 200 },
+			debug: false,
 		}
 	},
 	scene: [HelloWorldScene]
