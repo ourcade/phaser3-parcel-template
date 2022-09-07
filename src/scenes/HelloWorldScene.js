036a2287364552aeb581c6fce8825e69714dc3bf
Nicolas Nocete <nicolasnocete@gmail.com>
2022-09-07 16:10:04 -0300

    Se agrega imagen de ejemplo y EventCenter

--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -1,5 +1,20 @@
 import Phaser from 'phaser'
 
+
+// Manejador de eventos centralizados para comunicacion de componentes
+
+// Importacion
+//import { sharedInstance as events } from './EventCenter'
+
+// Emisor de mensaje de difusion
+// Recibe el nombre del mensaje y los valores de parametro
+// events.emit('health-changed', this.health)
+
+// Receptor de mensaje, por ejemplo escena de UI
+// Recibe el nombre del mensaje y una funcion callback a ejecutar
+// events.on('health-changed', this.handleHealthChanged, this)
+
+
 export default class HelloWorldScene extends Phaser.Scene
 {
 	constructor()
